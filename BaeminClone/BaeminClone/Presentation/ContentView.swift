3fbf87e8c906ba8f208e4b9209eead9e97d49033
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-12-08 17:20:19 +0900

    [setting] .gitignore 추가

---
+++ BaeminClone/BaeminClone/Presentation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaeminClone
+//
+//  Created by 조영서 on 12/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .font(.pretendard(.head_b_24))
+        }
+        .padding()
+    }
+}
